a2d4b1fdb9cdb04b6240803ef50c6b58184a8362
kaderbendjebbar <bendjebbar@gmail.com>
2024-02-21 22:51:20 +0100

    A

--- counter.js
+++ counter.js
@@ -1,1 +1,16 @@
+if (!localStorage.getItem("counter")) {
+  localStorage.setItem("counter", 0);
+}
 
+let counter = localStorage.getItem("counter");
+document.querySelector("#counter").innerHTML = counter;
+//document.addEventListener("DOMContentLoaded", () => {
+
+//document.querySelector("button").onclick = () => {
+//counter++;
+//  document.querySelector("#counter").innerHTML = counter;
+localStorage.setItem("counter", counter);
+// };
+
+//return false;
+//});
---
+++ creationButton.js
@@ -0,0 +1,4 @@
+var btn = document.createElement("BUTTON"); // Créer un élément <button>
+var t = document.createTextNode("CLICK ME"); // Créer un noeud textuel
+btn.appendChild(t); // Ajouter le texte au bouton
+document.body.appendChild(btn); // Ajoute la balise <button> à la balise <body>
